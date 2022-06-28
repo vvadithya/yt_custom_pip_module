addd21dee4a05126393d6a2e77db6069eafd39a1
vvadithya <vedulavenkataadithya@gmail.com>
2022-06-28 14:45:16 +0400

    inital setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="yt_downloaderr",
+    version="0.0.1",
+    author="Adithya",
+    author_email="sampleemail@gmail.com",
+    description="Package to download videos",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    install_requires  = ['Click','requests',...], # List all your dependencies inside the list
+    license = 'MIT'
+)
